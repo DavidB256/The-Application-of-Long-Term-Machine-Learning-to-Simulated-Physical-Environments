766d10320e8e5bfa2c9bf3a2369cee9610bdf5bb
DavidB256 <davidbass256@gmail.com>
2018-06-04 22:06:53 -0400

    Add files via upload

---
+++ physics_engine_v1.py
@@ -0,0 +1,82 @@
+from time import time
+
+# all measurements are in base metric units
+# shapes: rect=0, ellipse=1
+
+class Solid:
+    def __init__(self, pos_derivs, rot_derivs, height, width, mass, static):
+        self.height = height
+        self.width = width
+        self.mass = mass
+        self.static = static
+
+        # sets initial position and rotation
+        if pos_derivs is None:
+            self.pos_derivs = [[0, 0], [0, 0], [0, 9.81], [0, 0]]
+        else:
+            self.pos_derivs = pos_derivs
+        if rot_derivs is None:
+            self.rot_derivs = [[0, 0], [0, 0], [0, 0], [0, 0]]
+        else:
+            self.rot_derivs = rot_derivs
+
+    def update(self, t):
+        # update pos derivs from s to a and round out floating point errors
+        for i in range(2):
+            self.pos_derivs[0][i] += (self.pos_derivs[1][i] * t) + (.5 * self.pos_derivs[2][i] * t * t) + ((1/3) * self.pos_derivs[3][i] * t * t * t)
+        for i in range(2):
+            self.pos_derivs[1][i] += (self.pos_derivs[2][i] * t) + (.5 * self.pos_derivs[3][i] * t * t)
+        for i in range(2):
+            self.pos_derivs[2][i] += self.pos_derivs[3][i] * t
+        for i in range(4):
+            for j in range(2):
+                self.pos_derivs[i][j] = round(self.pos_derivs[i][j], 15)
+
+        # K LET'S ADD THE SAME THING BUT FOR ROTATION LATER
+
+class Rect(Solid):
+    def __init__(self, pos_derivs=None, rot_derivs=None, height=10, width=10, mass=10, static=False):
+        Solid.__init__(self, pos_derivs, rot_derivs, height, width, mass, static)
+
+        # initializes solid in output.txt
+        f.write('init' + '?' +
+                'shape' + '0' + '?' +
+                'pos_derivs' + str(self.pos_derivs) + '?' +
+                'rot_derivs' + str(self.rot_derivs) + '?' +
+                'height' + str(self.height) + '?' +
+                'width' + str(self.width) + '?\n')
+
+class Ellipse(Solid):
+    def __init__(self, pos_derivs=None, rot_derivs=None, height=10, width=10, mass=10, static=False):
+        Solid.__init__(self, pos_derivs, rot_derivs, height, width, mass, static)
+
+        # initializes solid in output.txt
+        f.write('init' + '?' +
+                'shape' + '1' + '?' +
+                'pos_derivs' + str(self.pos_derivs) + '?' +
+                'rot_derivs' + str(self.rot_derivs) + '?' +
+                'height' + str(self.height) + '?' +
+                'width' + str(self.width) + '?\n')
+
+start_time = time()
+
+tick_len = .05
+seconds = 8
+f = open('output.txt', 'w')
+
+solids = []
+# solids.append(Rect())
+solids.append(Rect(pos_derivs=[[250, 250], [10, -30], [0, 9.81], [0, 0]], height=5, width=5))
+# solids.append(Rect(pos_derivs=[[499, 30], [0, 0], [0, 0], [0, 0]], height=50, width=50))
+solids.append(Ellipse(pos_derivs=[[100, 100], [5, 0], [0, 0], [0, 0]]))
+
+# run simulation to output into output.txt, number of lines of output.txt equals len(solids)+len(solids)(seconds/tick_len)
+for i in range(int(seconds / tick_len)):
+    for j in range(len(solids)):
+        # creates txt output
+        f.write(str(j) + '?' +
+                'pos_derivs' + str(solids[j].pos_derivs) + '?\n')
+        solids[j].update(tick_len)
+
+
+print('time elapsed:', time() - start_time)
