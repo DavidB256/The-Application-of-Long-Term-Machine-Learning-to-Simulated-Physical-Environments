1ede28f9e09ce23ef2818ae1f164fde7b844773c
David Bass <davidbass256@gmail.com>
2018-07-29 23:11:50 -0400

    Add files via upload

--- physics_engine.py
+++ physics_engine.py
@@ -3,106 +3,118 @@ from math import sin, cos, radians, sqrt
 
 # all measurements are in base metric units, all angles are in radians
 
+# compare two border lines and return true if they intersect
+def intersect(border1, border2):
+    if (border1.__class__.__name__ == 'Vertical' != border2.__class__.__name__) or \
+       (border1.__class__.__name__ == 'Line' and border2.__class__.__name__ == 'Arc'):
+        temp = border1
+        border1 = border2
+        border2 = temp
+
+    if border1.shape == 'line':
+        # finds if the intersection of the lines lies within their domains
+        if border2.shape == 'line':
+            try:
+                x = (border2.y_intercept - border1.y_intercept) / (border1.slope - border2.slope)
+            # if the lines are parallel, the difference of their slopes would be 0, divide by 0 = :(
+            except ZeroDivisionError:
+                return False
+            return border1.domain[0] <= x <= border1.domain[1] and border2.domain[0] <= x <= border2.domain[1]
+
+        # finds if the intersection of the lines lies within the domain of the sloped line and the range of the vertical line
+        elif border2.shape == 'vertical':
+            return border1.domain[0] <= border2.x_coord <= border1.domain[1] and \
+                   border2.y_range[0] <= border1.slope * border2.x_coord + border1.y_intercept <= border2.y_range[1]
+
+        # finds if the line passes through the circle or if one of its endpoints lies within the circle
+        elif border2.shape == 'circle':
+            # this convoluted formula finds the x coordinate of the point of intersection of a line and the shortest path (a perpendicular
+            # line) to a certain point (the center of the circle)
+            x = (border2.center[0] - border1.y_intercept * border1.slope + border2.center[1] * border1.slope) / (
+            border1.slope ** 2 + 1)
+            y = border1.slope * x + border1.y_intercept
+
+            return (border1.domain[0] <= x <= border1.domain[1] and
+                    sqrt((x - border2.center[0]) ** 2 + (y - border2.center[1]) ** 2) <= border2.radius) or \
+                   sqrt((border1.domain[0] - border2.center[0]) ** 2 + (
+                   border1.slope * border1.domain[0] + border1.y_intercept - border2.center[1]) ** 2) \
+                   <= border2.radius or \
+                   sqrt((border1.domain[1] - border2.center[0]) ** 2 + (
+                   border1.slope * border1.domain[1] + border1.y_intercept - border2.center[1]) ** 2) \
+                   <= border2.radius
+
+    # test vertical with the three shapes
+    elif border1.shape == 'vertical':
+        # finds if the intersection of the lines lies within the domain of the sloped line and the range of the vertical line
+        if border2.shape == 'line':
+            return border2.domain[0] <= border1.x_coord <= border2.domain[1] and \
+                   border1.y_range[0] <= border2.slope * border1.x_coord + border2.y_intercept <= border1.y_range[1]
+
+        # finds if the verticals have the same x coordinate and have overlapping ranges
+        elif border2.shape == 'vertical':
+            return border1.x_coord == border2.x_coord and (border2.y_range[0] <= border1.y_range[0] <= border2.y_range[1] or
+                                                      border2.y_range[0] <= border1.y_range[1] <= border2.y_range[1] or
+                                                      border1.y_range[0] <= border2.y_range[0] <= border1.y_range[1])
+
+        # finds if the vertical passes through the circle or if one of its endpoints lies within the circle
+        elif border2.shape == 'circle':
+            return (border1.y_range[0] <= border2.center[1] <= border1.y_range[1] and border2.center[
+                0] - border1.x_coord <= border2.radius) or \
+                   sqrt((border1.x_coord - border2.center[0]) ** 2 + (
+                   border1.y_range[0] - border2.center[1]) ** 2) <= border2.radius or \
+                   sqrt((border1.x_coord - border2.center[0]) ** 2 + (
+                   border1.y_range[1] - border2.center[1]) ** 2) <= border2.radius
+
+    # test circle with the three shapes
+    elif border1.shape == 'circle':
+        # finds if the line passes through the circle or if one of its endpoints lies within the circle
+        if border2.shape == 'line':
+            # this convoluted formula finds the x coordinate of the point of intersection of a line and the shortest path (a perpendicular
+            # line) to a certain point (the center of the circle)
+            x = (border1.center[0] - border2.y_intercept * border2.slope + border1.center[1] * border2.slope) / (
+            border2.slope ** 2 + 1)
+            y = border2.slope * x + border2.y_intercept
+
+            return (border2.domain[0] <= x <= border2.domain[1] and
+                    sqrt((x - border1.center[0]) ** 2 + (y - border1.center[1]) ** 2) <= border1.radius) or \
+                   sqrt((border2.domain[0] - border1.center[0]) ** 2 +
+                        (border2.slope * border2.domain[0] + border2.y_intercept - border1.center[1]) ** 2) <= border1.radius or \
+                   sqrt((border2.domain[1] - border1.center[0]) ** 2 +
+                        (border2.slope * border2.domain[1] + border2.y_intercept - border1.center[1]) ** 2) <= border1.radius
+
+        # finds if the vertical passes through the circle or if one of its endpoints lies within the circle
+        elif border2.shape == 'vertical':
+            return (border2.y_range[0] <= border1.center[1] <= border2.y_range[1] and border1.center[
+                0] - border2.x_coord <= border1.radius) or \
+                   sqrt((border2.x_coord - border1.center[0]) ** 2 + (
+                   border2.y_range[0] - border1.center[1]) ** 2) <= border1.radius or \
+                   sqrt((border2.x_coord - border1.center[0]) ** 2 + (
+                   border2.y_range[1] - border1.center[1]) ** 2) <= border1.radius
+
+        # finds if the distance between the circles' centers is less than or equal to the sum of their radii
+        elif border2.shape == 'circle':
+            return sqrt((border1.center[0] - border2.center[0]) ** 2 + (
+            border1.center[1] - border2.center[1]) ** 2) <= border1.radius + border2.radius
+
 class Line:
     # shape can be 'line', 'vertical', or 'circle'
     # the 2nd, 3rd, and 4th lines of the following __init__ declaration contain the instance variables exclusively for
     # 'line', 'vertical', and 'circle', respectively
     # shape is the instance variable that is required for all three types, because it specifies the type
-    def __init__(self, shape,
-                 slope=None, y_intercept=None, domain=None,
-                 x_coord=None, y_range=None,
-                 radius=None, center=None):
+    def __init__(self, slope=None, y_intercept=None, domain=None):
+        self.slope = slope
+        self.y_intercept = y_intercept
+        self.domain =domain
 
-        self.shape = shape
+class Vertical:
+    def __init__(self, x_coord=None, y_range=None,):
+        self.x_coord = x_coord
+        self.y_range = y_range
 
-        if shape == 'line':
-            self.slope = slope
-            self.y_intercept = y_intercept
-            self.domain =domain
-        elif shape == 'vertical':
-            self.x_coord = x_coord
-            self.y_range = y_range
-        elif shape == 'circle':
-            self.radius = radius
-            self.center = center
-
-    # returns true if the lines are intersecting, has one if statement for each of the 9 (3 x 3) combinations
-    def intersect(self, other):
-        # test line with the three shapes
-        if self.shape == 'line':
-            # finds if the intersection of the lines lies within their domains
-            if other.shape == 'line':
-                try:
-                    x = (other.y_intercept - self.y_intercept) / (self.slope - other.slope)
-                # if the lines are parallel, the difference of their slopes would be 0, divide by 0 = :(
-                except ZeroDivisionError:
-                    return False
-                return self.domain[0] <= x <= self.domain[1] and other.domain[0] <= x <= other.domain[1]
-
-            # finds if the intersection of the lines lies within the domain of the sloped line and the range of the vertical line
-            elif other.shape == 'vertical':
-                return self.domain[0] <= other.x_coord <= self.domain[1] and \
-                       other.y_range[0] <= self.slope * other.x_coord + self.y_intercept <= other.y_range[1]
-
-            # finds if the line passes through the circle or if one of its endpoints lies within the circle
-            elif other.shape == 'circle':
-                # this convoluted formula finds the x coordinate of the point of intersection of a line and the shortest path (a perpendicular
-                # line) to a certain point (the center of the circle)
-                x = (other.center[0] - self.y_intercept * self.slope + other.center[1] * self.slope) / (self.slope ** 2 + 1)
-                y = self.slope * x + self.y_intercept
-
-                return (self.domain[0] <= x <= self.domain[1] and
-                       sqrt((x - other.center[0]) ** 2 + (y - other.center[1]) ** 2) <= other.radius) or \
-                       sqrt((self.domain[0] - other.center[0]) ** 2 + (self.slope * self.domain[0] + self.y_intercept - other.center[1]) ** 2)\
-                       <= other.radius or \
-                       sqrt((self.domain[1] - other.center[0]) ** 2 + (self.slope * self.domain[1] + self.y_intercept - other.center[1]) ** 2)\
-                       <= other.radius
-
-        # test vertical with the three shapes
-        elif self.shape == 'vertical':
-            # finds if the intersection of the lines lies within the domain of the sloped line and the range of the vertical line
-            if other.shape == 'line':
-                return other.domain[0] <= self.x_coord <= other.domain[1] and \
-                       self.y_range[0] <= other.slope * self.x_coord + other.y_intercept <= self.y_range[1]
-
-            # finds if the verticals have the same x coordinate and have overlapping ranges
-            elif other.shape == 'vertical':
-                return self.x_coord == other.x_coord and (other.y_range[0] <= self.y_range[0] <= other.y_range[1] or
-                                                          other.y_range[0] <= self.y_range[1] <= other.y_range[1] or
-                                                          self.y_range[0] <= other.y_range[0] <= self.y_range[1])
-
-            # finds if the vertical passes through the circle or if one of its endpoints lies within the circle
-            elif other.shape == 'circle':
-                return (self.y_range[0] <= other.center[1] <= self.y_range[1] and other.center[0] - self.x_coord <= other.radius) or \
-                       sqrt((self.x_coord - other.center[0]) ** 2 + (self.y_range[0] - other.center[1]) ** 2) <= other.radius or \
-                       sqrt((self.x_coord - other.center[0]) ** 2 + (self.y_range[1] - other.center[1]) ** 2) <= other.radius
-
-        # test circle with the three shapes
-        elif self.shape == 'circle':
-            # finds if the line passes through the circle or if one of its endpoints lies within the circle
-            if other.shape == 'line':
-                # this convoluted formula finds the x coordinate of the point of intersection of a line and the shortest path (a perpendicular
-                # line) to a certain point (the center of the circle)
-                x = (self.center[0] - other.y_intercept * other.slope + self.center[1] * other.slope) / (other.slope ** 2 + 1)
-                y = other.slope * x + other.y_intercept
-
-                return (other.domain[0] <= x <= other.domain[1] and
-                       sqrt((x - self.center[0]) ** 2 + (y - self.center[1]) ** 2) <= self.radius) or \
-                       sqrt((other.domain[0] - self.center[0]) ** 2 +
-                       (other.slope * other.domain[0] + other.y_intercept - self.center[1]) ** 2) <= self.radius or \
-                       sqrt((other.domain[1] - self.center[0]) ** 2 +
-                       (other.slope * other.domain[1] + other.y_intercept - self.center[1]) ** 2) <= self.radius
-
-            # finds if the vertical passes through the circle or if one of its endpoints lies within the circle
-            elif other.shape == 'vertical':
-                return (other.y_range[0] <= self.center[1] <= other.y_range[1] and self.center[0] - other.x_coord <= self.radius) or \
-                       sqrt((other.x_coord - self.center[0]) ** 2 + (other.y_range[0] - self.center[1]) ** 2) <= self.radius or \
-                       sqrt((other.x_coord - self.center[0]) ** 2 + (other.y_range[1] - self.center[1]) ** 2) <= self.radius
-
-            # finds if the distance between the circles' centers is less than or equal to the sum of their radii
-            elif other.shape == 'circle':
-                return sqrt((self.center[0] - other.center[0]) ** 2 + (self.center[1] - other.center[1]) ** 2) <= self.radius + other.radius
-
+class Arc:
+    def __init__(self, center, radius):
+        self.center = center
+        self.radius = radius
 
 class Rect:
     def __init__(self, pos, velocity, tilt=0., angular_v=0., height=10., width=10., mass=1., material=0, static=False, f_coeff=0.):
@@ -127,6 +139,9 @@ class Rect:
 
         # will contain 4 Line objects for each of the 4 border lines of the rectangle, set in the update method
         self.borders = [None for i in range(4)]
+
+        # the length of the diagnol of the rectangle, used as a filter to prevent collision testing with two far-apart rectangles
+        self.diagonal = sqrt(height ** 2 + width ** 2)
 
     def update(self, t):
         # udpate position and tilt based on linear and rotational velocities
